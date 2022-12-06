2975fe8cc70dd95a28d4ea1c054f1259f179599c
HadziqM <50319538+HadziqM@users.noreply.github.com>
2022-12-06 15:57:08 +0700

    test chrono

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod crud;
 
-// use chrono::prelude::*;
+use chrono::prelude::*;
 // use chrono::serde::ts_seconds::serialize as to_ts;
 // use reqwest;
 use serde::{Deserialize, Serialize};
@@ -42,34 +42,36 @@ struct ForVect {
 
 fn main() {
     let user = crud::Collection {
-        host: "http://192.168.0.110".to_string(),
+        host: "http://127.0.0.1".to_string(),
         port: 8090,
-        collection: "user".to_string(),
+        collection: "test".to_string(),
     };
-    let mydata: CreateUser = CreateUser {
-        username: "now2".to_string(),
-        field: "Pro".to_string(),
-        money: 100,
-    };
-    let data: String = serde_json::to_string(&mydata).unwrap();
+    // let mydata: CreateUser = CreateUser {
+    //     username: "now2".to_string(),
+    //     field: "Pro".to_string(),
+    //     money: 100,
+    // };
+    // let data: String = serde_json::to_string(&mydata).unwrap();
+    // let created_data: ForVect = serde_json::from_str(&created).unwrap();
+    // println!("example of list {}", user.list(None));
+    let now = Utc::now().format("%+");
+    let data = ["{\"field\":\"", format!("{}", now).as_ref(), "\"}"].concat();
     let created = user.create(data);
-    let created_data: ForVect = serde_json::from_str(&created).unwrap();
     println!("example of create {}", &created);
-    println!("example of list {}", user.list(None));
-    println!("{}", created_data.id);
-    println!(
-        "example of update{}",
-        user.update(
-            String::from(&created_data.id),
-            String::from(r#"{"money":1234}"#)
-        )
-    );
-    println!(
-        "example of select {}",
-        user.select(String::from(&created_data.id))
-    );
-    println!(
-        "example of delete {}",
-        user.delete(String::from(&created_data.id))
-    );
+    // println!("{}", created_data.id);
+    // println!(
+    //     "example of update{}",
+    //     user.update(
+    //         String::from(&created_data.id),
+    //         String::from(r#"{"money":1234}"#)
+    //     )
+    // );
+    // println!(
+    //     "example of select {}",
+    //     user.select(String::from(&created_data.id))
+    // );
+    // println!(
+    //     "example of delete {}",
+    //     user.delete(String::from(&created_data.id))
+    // );
 }
